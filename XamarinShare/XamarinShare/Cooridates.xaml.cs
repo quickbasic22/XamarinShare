aef78722fb65d2b93f092a23aadb1f10c70b25a7
David Morrow <quickbasic44@outlook.com>
2022-06-05 19:52:01 -0400

    Add project files.

---
+++ XamarinShare/XamarinShare/Cooridates.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinShare
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cooridates : ContentPage
+    {
+        public Cooridates()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            var lat1 = double.Parse(Latitude1.Text);
+            var lng1 = double.Parse(Longitude1.Text);
+            var lat2 = double.Parse(Latitude2.Text);
+            var lng2 = double.Parse(Longitude2.Text);
+
+            var result = Xamarin.Essentials.UnitConverters.CoordinatesToMiles(lat1, lng1, lat2, lng2);
+            Results.Text = result.ToString() + " Miles";
+        }
+    }
+}
---
+++ XamarinShare/XamarinShare/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XamarinShare
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var text = TextToShare.Text;
+            await Share.RequestAsync(text, "Share your text");
+            await Navigation.PushAsync(new Cooridates());
+        }
+    }
+}
